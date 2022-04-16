f8fdbfe75f42d12f9e8d19dae4b29fc4cd54d6ef
achrafsahnoun1 <achraf.sahnoun@studenti.unipd.it>
2022-04-16 16:49:09 +0200

    mern

--- script.groovy
+++ script.groovy
@@ -7,6 +7,7 @@ def buildFront() {
     dir ('../frontend') { 
     sh 'rm -R package-lock.json'
     sh 'npm cache clean -force'
+    sh 'npm init'
     sh "npm install -g @angular/cli"
     sh "npm install"
     echo "building of the frontend part successful ..."
