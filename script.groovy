e348d5c6c8094a0623bb9660311c605701f264b6
achrafsahnoun1 <49032031+achrafsahnoun1@users.noreply.github.com>
2022-05-01 18:00:07 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -24,8 +24,9 @@ def buildBack() {
 def buildImage() {
     echo "building the docker image"
     dir ('../mern-app/frontend/') { 
+    sh 'pwd'
     //withCredentials([usernamePassword(credentialsId: 'docker_hub_repo', usernameVariable: 'USER', passwordVariable: 'PASS')]){
-    //sh 'docker build -t tsah .'
+    sh 'docker build -t frontend .'
     //sh "docker login -u ${USER} -p ${PASS}"
     //sh 'docker push tsah007/bootcamp:jma-1.1'
     }
